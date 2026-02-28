05d31cffa61a37eb58fdc959afed922d15272a73
johndoe <simon.gaspar@pm.me>
2026-02-28 19:02:18 +0100

    feat: add marker clustering to handle overlapping swiss tournaments

--- src/components/features/TournamentMap.tsx
+++ src/components/features/TournamentMap.tsx
@@ -3,7 +3,10 @@
 import { useEffect, useState, useMemo, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import L from "leaflet";
+import MarkerClusterGroup from 'react-leaflet-cluster';
 import { Tournament } from "@/lib/types/tournament";
 
 // Custom premium marker icon using divIcon for Tailwind styling and pulsing effect
@@ -195,7 +198,14 @@ export default function TournamentMap({ tournaments, hoveredId, onBoundsChange, 
             <MapUpdater center={center} zoom={zoom} />
             <MapEvents onBoundsChange={onBoundsChange} tournaments={tournaments} />
 
-            {markers}
+            <MarkerClusterGroup
+                chunkedLoading
+                spiderfyOnMaxZoom={true}
+                showCoverageOnHover={false}
+                maxClusterRadius={40}
+            >
+                {markers}
+            </MarkerClusterGroup>
         </MapContainer>
     );
 }
