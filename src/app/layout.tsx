87b0650fddb45427ecbec24f187105f3b0f95817
Simon Gaspar <simon.gaspar.du.76@gmail.com>
2026-03-02 21:13:50 +0100

    fix: hardcode Clarity ID vohjie4ukh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,17 +53,15 @@ export default function RootLayout({
           {children}
         </Providers>
 
-        {process.env.NEXT_PUBLIC_CLARITY_ID && (
-          <Script id="clarity-script" strategy="afterInteractive">
-            {`
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
-            `}
-          </Script>
-        )}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vohjie4ukh");
+          `}
+        </Script>
       </body>
     </html>
   );
